45ac956b26f4401a87616326cb91e32ab72e1383
david <d@vidc.dk>
2018-01-08 23:01:22 +0100

    Update initial picks

--- designs.js
+++ designs.js
@@ -1,10 +1,21 @@
 // Select color input
 // Select size input
 
+var colorpick = $("colorPicker").val();
+
+var inputH = $("#input_height").val();
+
+var inputW = $("#input_width").val();
+
+console.log(inputW);
+console.log(inputH);
+console.log(colorPick);
+
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
 
 }
