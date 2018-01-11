7ea2eef596fd168c3ab9d9d33320a65a833dc559
david <d@vidc.dk>
2018-01-11 22:34:41 +0100

    started the makegrid function

--- designs.js
+++ designs.js
@@ -1,28 +1,45 @@
 // Select color input
+
+var colorInput = $(colorPicker);
+//console.log(colorInput);
 // Select size input
+var heightInput = $("#input_height").val();
+//console.log(heightInput);
+var widthInput = $("#input_width").val();
+//console.log(widthInput);
+// Select table element to create Grid
+var grid = $("#pixel_canvas");
 
-var colorpick = $("colorPicker").val();
+/*
+When size is submitted by the user, call makeGrid()
+*/
 
-var inputH = $("#input_height").val();
+$("#submit_button").click(function() {
+    makeGrid();
 
-var inputW = $("#input_width").val();
-
-console.log(inputW);
-console.log(inputH);
-console.log(colorPick);
-
-
-// When size is submitted by the user, call makeGrid()
+})
 
 
-$("#input").on("click", function() {
-  
-  
-} )
-
+// The makeGrid() function
 
 function makeGrid() {
+    var row = heightInput;
+    var column = widthInput;
 
-    // Your code goes here!
+    //CleanSlate - del all previous
+    grid.children().remove();
+
+    //actually making the grid
+    for (var i = 0; i < row; i++) {
+
+        grid.append("<tr></tr>");
+    }
+
+    for (var n = 0; n < column; n++) {
+
+        grid.children().append("<td></td>");
+
+
+    }
 
 }
